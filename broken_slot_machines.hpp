0c1b236291bcda6550909a9ea31b824c05cf4ac5
nel215 <otomo.yuhei@gmail.com>
2018-03-15 23:48:26 +0900

    Implement quick play

--- broken_slot_machines.hpp
+++ broken_slot_machines.hpp
@@ -6,6 +6,10 @@ PlaySlotsClass PlaySlots;
 class BrokenSlotMachines {
  public:
   int playSlots(int coins, int maxTime, int noteTime, int numMachines) {
+    for (int i=0; i < 100; i++) {
+      int win = PlaySlots.quickPlay(0, 1);
+      cerr << win << endl;
+    }
     return 0;
   }
 };
--- play_slots.hpp
+++ play_slots.hpp
@@ -2,15 +2,53 @@
 #define PLAY_SLOTS_HPP_
 #include<string>
 #include<vector>
+#include<utility>
 using namespace std;  // noqa
 
 class PlaySlotsClass {
+  int randint(int b) {
+    // TODO: good rng
+    return rand() % b;
+  }
+  pair<int, vector<string>> doPlay(int machineNumber, int times) {
+    int win = 0;
+    vector<string> res;
+    for (int i = 0; i < times; i++) {
+      if (coins <= 0) {
+        throw "Attempted to play after coins ran out.";
+      }
+      coins--;
+      int w = wheels[machineNumber][0].size();
+      vector<int> idx = {randint(w), randint(w), randint(w)};
+      string s = "";
+      for (int j = -1; j <= 1; j++) {
+        for (int k = 0; k < 3; k++) {
+          s += wheels[machineNumber][k][(idx[k] + w + j)%w];
+        }
+      }
+      res.push_back(s);
+      if ((s[3] != s[4]) || (s[3] != s[5])) {
+        continue;
+      }
+      if (s[3] == 'A') win += 1000;
+      if (s[3] == 'B') win +=  200;
+      if (s[3] == 'C') win +=  100;
+      if (s[3] == 'D') win +=   50;
+      if (s[3] == 'E') win +=   20;
+      if (s[3] == 'F') win +=   10;
+      if (s[3] == 'G') win +=    5;
+    }
+    coins += win;
+    return make_pair(win, res);
+  }
+
  public:
   int coins, maxTime, noteTime, numMachines;
+  vector<vector<string>> wheels;
   void initialize() {
     cin >> coins >> maxTime >> noteTime >> numMachines;
     cerr << coins << " " << maxTime << " " << noteTime << " " << numMachines << endl;
-    vector<vector<string>> wheels;
+    wheels.clear();
     for (int i=0; i < numMachines; i++) {
       vector<string> wheel(3);
       for (int j=0; j < 3; j++) {
@@ -20,7 +58,13 @@ class PlaySlotsClass {
       wheels.push_back(wheel);
     }
   }
-  int quickPlay(int machineNumber, int times);
+  int quickPlay(int machineNumber, int times) {
+    if (times > maxTime) {
+      throw "Attempted to play after time ran out.";
+    }
+    maxTime -= times;
+    return doPlay(machineNumber, times).first;
+  }
   vector<string> notePlay(int machineNumber, int times);
 };
 
