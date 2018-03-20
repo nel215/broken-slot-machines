04e5111951297d2dfbedcfa8af24e59d4a831d71
nel215 <otomo.yuhei@gmail.com>
2018-03-20 23:00:52 +0900

    Add gp minimamizer

--- run_experiment.py
+++ run_experiment.py
@@ -1,27 +1,53 @@
 import os
 import subprocess
+import numpy as np
+from tqdm import tqdm
 from datetime import datetime
 from logging import getLogger, basicConfig, INFO
 from concurrent.futures import ProcessPoolExecutor
+from skopt import gp_minimize
+from skopt.space import Real
 logger = getLogger(__name__)
 
 
 def run_experiment(args):
-    fpath, logdir = args
+    fpath, logdir, x = args
     fname = os.path.basename(fpath).split('.')[0]
     lpath = os.path.join(logdir, '{}.ltsv'.format(fname))
     with open(fpath) as inp_fp, open(lpath, 'w') as log_fp:
         args = ['./a.out']
-        args += ['win-prior', '10']
-        args += ['win-count', '0.1']
-        args += ['min-exp', '0.8']
-        args += ['min-var', '0.2']
+        args += ['win-prior', str(x[0])]
+        args += ['win-count', str(x[1])]
+        args += ['min-exp', str(x[2])]
+        args += ['min-var', str(x[3])]
         res = subprocess.run(
             args, stdin=inp_fp, stderr=log_fp)
-        logger.info('{}:{}'.format(fpath, res.returncode))
 
 
-def main():
+def parse_log(args):
+    fpath, logdir, _ = args
+    fname = os.path.basename(fpath).split('.')[0]
+    lpath = os.path.join(logdir, '{}.ltsv'.format(fname))
+
+    res = {}
+    with open(lpath) as fp:
+        for row in fp:
+            d = {}
+            for chunk in row.strip().split('\t'):
+                k, v = chunk.split(':')
+                d[k] = v
+            tag = d.get('tag')
+            if tag == 'optimal':
+                res['optimal'] = float(d['optimal'])
+            if tag == 'result':
+                res['result'] = float(d['coins'])
+
+    regret = res['result'] / res['optimal']
+    return regret
+
+
+def objective(x):
+    logger.info('x: {}'.format(x))
     basicConfig(level=INFO)
     now = datetime.now()
     logdir = os.path.join('./log', now.strftime('%Y%m%d-%H%M%S'))
@@ -29,13 +55,41 @@ def main():
     executor = ProcessPoolExecutor()
     inpdir = './testcase/'
     args_list = []
-    for fname in os.listdir(inpdir):
+    total = 20
+    for fname in sorted(os.listdir(inpdir)):
         if fname.find('.in') == -1:
             continue
         fpath = os.path.join(inpdir, fname)
-        args_list.append([fpath, logdir])
-    for res in executor.map(run_experiment, args_list):
+        args_list.append([fpath, logdir, x])
+    args_list = args_list[:total]
+    for res in tqdm(executor.map(run_experiment, args_list), total=total, ncols=0):
         pass
+
+    regrets = []
+    for res in tqdm(executor.map(parse_log, args_list), total=total, ncols=0):
+        regrets.append(res)
+
+    mean = np.mean(regrets)
+    std = np.std(regrets)
+    logger.info('mean: {}, std: {}'.format(mean, std))
+    return -mean
+
+
+def optimize():
+    space = [
+        Real(1.0, 30.0, name='win_prior'),
+        Real(0.05, 10.0, name='win_count'),
+        Real(0.25, 1.0, name='min_exp'),
+        Real(0.01, 0.5, name='min_var'),
+    ]
+
+    x0 = [10, 0.1, 0.8, 0.2]
+    res = gp_minimize(objective, space, x0=x0, n_calls=11, random_state=215)
+    logger.info('best mean: {}, best x: {}'.format(-res.fun, res.x))
+
+
+def main():
+    optimize()
 
 
 if __name__ == '__main__':
